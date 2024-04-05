dc8dea8b100aa8e3044a33956e2eea1c50226135
Miguel Leiva <migueleiva@gmail.com>
2024-04-04 22:00:06 -0500

    Agregando output

--- ejercicio-mejorado/ec2.tf
+++ ejercicio-mejorado/ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "mi_servidor" {
   ami                    = "ami-xxxxxxxxx"
   instance_type          = "t3.large"
   #vpc_security_group_ids = "mifireall" 
+  #depedencia implicita
   vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
 
   user_data = <<-EOF
@@ -13,4 +14,9 @@ resource "aws_instance" "mi_servidor" {
               echo "Hola Grupo de Terraformers!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+
+  #dependencia explicita
+  depends_on = [
+    aws_security_group.mi_grupo_de_seguridad
+  ]
 }
---
+++ ejercicio-variables/outputs.tf
@@ -0,0 +1,3 @@
+output "instance_public_ip" {
+  value = aws_instance.vm_instance_imp.public_ip
+}
